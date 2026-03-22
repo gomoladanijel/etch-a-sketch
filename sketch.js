c3ea6ee24b54d70713218b9b0d52b36dcca17ef3
Danijel Gomola <266135542+gomoladanijel@users.noreply.github.com>
2026-03-22 01:47:53 +0100

    Add sketch.js

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const grid = document.querySelector(".grid");
+const createGridBtn = document.querySelector(".create-grid-btn");
+const resetBtn = document.querySelector(".reset-btn");
+
+const DEFAULT_GRID_SIZE = 10;
+
+function changeColor() {
+    const RED = Math.floor(Math.random() * 255);
+    const GREEN = Math.floor(Math.random() * 255);
+    const BLUE = Math.floor(Math.random() * 255);
+
+    return `rgb(${RED}, ${GREEN}, ${BLUE})`;
+}
+
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+
+            cell.style.width = `${480 / size}px`;
+            cell.style.height = `${480 / size}px`;
+
+            grid.appendChild(cell);
+
+            cell.addEventListener("mouseover", () => {
+                cell.style.backgroundColor = changeColor();
+            })
+
+            resetBtn.addEventListener("click", () => {
+                cell.style.backgroundColor = "#fff";
+            });
+        }
+    }
+}
+
+createGrid(DEFAULT_GRID_SIZE);
+
+createGridBtn.addEventListener("click", () => {
+    let gridSize;
+
+    do {
+        gridSize = prompt("What number of squares per side do you want (max of 100)?");
+
+        if (gridSize == null || gridSize === "") {
+            break;
+        }
+    } while (!Number.isInteger(+gridSize) || +gridSize <= 0 || +gridSize > 100);
+
+    grid.textContent = "";
+    createGrid(+gridSize);
+});
